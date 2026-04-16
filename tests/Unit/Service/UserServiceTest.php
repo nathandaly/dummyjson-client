3ce43e0a17176d79fc694276e2377c38b906e921
Nate <nathandalypretoria@hotmail.co.uk>
2026-04-16 11:46:28 +0100

    :test_tube: Outlines test suite for UserService.

--- tests/Unit/Service/UserServiceTest.php
+++ tests/Unit/Service/UserServiceTest.php
@@ -10,7 +10,32 @@ use Tests\TestCase;
 class UserServiceTest extends TestCase
 {
     #[Test]
-    public function it_has_a_test_class(): void
+    public function it_retrieves_a_single_user_by_id(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function it_retrieves_a_paginated_list_of_users(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function it_created_a_user_and_returns_a_user_id(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function it_posts_required_fields_when_creating_a_user(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function it_maps_the_response_data_to_dto_with_validations(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function it_wraps_transport_failure_in_a_domain_exception(): void
     {
         $this->assertTrue(true);
     }
